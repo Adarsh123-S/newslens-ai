491986d93abb9b214ddd919cb252de469c6be822
Adarsh123-S <adarshsadarsh06@gmail.com>
2026-08-19 19:15:12 +0530

    Replace broken Reuters feed with The Guardian World feed

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ Add or remove RSS feeds here. Mix of sources is what enables
 RSS_FEEDS = {
     "The Hindu": "https://www.thehindu.com/news/national/feeder/default.rss",
     "Times of India (Top Stories)": "https://timesofindia.indiatimes.com/rssfeedstopstories.cms",
-    "Reuters World": "https://news.google.com/rss/search?q=when:24h+allinurl:reuters.com&ceid=US:en&hl=en-US&gl=US",
+    "The Guardian (World)": "https://www.theguardian.com/world/rss",
     "BBC World": "http://feeds.bbci.co.uk/news/world/rss.xml",
     "TechCrunch": "https://techcrunch.com/feed/",
     "Al Jazeera": "https://www.aljazeera.com/xml/rss/all.xml",
